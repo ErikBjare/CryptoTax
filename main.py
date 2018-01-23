9efbc861e540d99ca7591e27076b56c1e7e4afb9
Erik Bjäreholt <erik@bjareho.lt>
2018-01-23 16:25:44 +0100

    implemented reduce_trades to help reduce clutter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from typing import List, Dict, Any
+from functools import reduce
+
+import dateutil.parser
 
 # import sqlite3
 # connection = sqlite3.connect(':memory:')
@@ -22,17 +25,57 @@ def _format_csv_from_kraken(trades_csv):
     "Format a CSV from a particular source into a canonical data format"
     for trade in trades_csv:
         trade["pair"] = (trade["pair"][:4], trade["pair"][4:])
+        trade["time"] = dateutil.parser.parse(trade["time"])
+        trade["price"] = float(trade["price"])
+        trade["vol"] = float(trade["vol"])
+        trade["cost"] = float(trade["cost"])
     return trades_csv
 
 
-def _print_trades(trades):
+def _print_trades(trades, n=None):
     h = dict(zip(trades[0].keys(), trades[0].keys()))
     print(f"{h['time']:10.10}  {h['pair']:12.12}  {h['price']:10.10}  {h['cost']:10.10}")
-    for d in trades[:3]:
-        print(f"{d['time']:10.10}  {' / '.join(d['pair']):12.12}  {d['price']:10.10}  {d['cost']:10.10}")
+    for d in (trades[:n] if n else trades):
+        print(f"{d['time'].isoformat():10.10}  {' / '.join(d['pair']):12.12}  {d['price']:10.10}  {d['cost']:10.10}")
+
+
+def _sum_trades(t1, t2):
+    # Price becomes volume-weighted average
+    t1["price"] = (t1["price"] * t1["vol"] + t2["price"] * t2["vol"]) / (t1["vol"] + t2["vol"])
+    # Volume becomes sum of trades
+    t1["vol"] += t2["vol"]
+    t1["cost"] += t2["cost"]
+    return t1
+
+
+def test_sum_trades():
+    t1 = {"price": 1, "vol": 1, "cost": 1}
+    t2 = {"price": 2, "vol": 1, "cost": 2}
+    t3 = _sum_trades(t1, t2)
+    assert t3["price"] == 1.5
+    assert t3["vol"] == 2
+    assert t3["cost"] == 3
+
+
+def _reduce_trades(trades):
+    """Merges consequtive trades in the same pair on the same day"""
+    def r(processed, next):
+        if len(processed) == 0:
+            processed.append(next)
+        else:
+            last = processed[-1]
+            if last["time"].date() == next["time"].date() and \
+               last["pair"] == next["pair"]:
+                processed[-1] = _sum_trades(last, next)
+            else:
+                processed.append(next)
+        return processed
+    return reduce(r, trades, [])
 
 
 if __name__ == "__main__":
     trades_csv = _load_csv("data_private/kraken-trades.csv")
     trades = _format_csv_from_kraken(trades_csv)
+    # print(trades[0].keys())
+    trades = _reduce_trades(trades)
     _print_trades(trades)
